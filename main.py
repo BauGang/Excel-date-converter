94a6e25ed55bc3952ef768f7f05ef153631055dc
BauGang <35534811+BauGang@users.noreply.github.com>
2020-11-06 14:25:15 +0100

    Added some things

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ xlsx_file = Path('add_folder_here', 'add_file_here')
 wb_obj = openpyxl.load_workbook(xlsx_file)
 ws = wb_obj.active
 
+HoursOfWork = 7
+
 def convertDate(ordinal):
     epochStart = datetime(1899, 12, 31)
     if ordinal is not None:
@@ -18,12 +20,12 @@ def convertDate(ordinal):
 row_count = ws.max_row-284
 count = 0
 
-for j in range(1, row_count, 7):
+for j in range(1, row_count, HoursOfWork):  # skip the hours, so he can take the day 
     a = ws.cell(row=j, column=1).value
     b = convertDate(a)
     if giorno == str(b):
         for x in range(j+1, 140):           # you can put ws.max_row but remember, the librarie take the last row changed,  
-            if count == 6:                  # so if you had 300 rows but you edited the row 1000 too the librarie take the row 1000.
+            if count == HoursOfWork:        # so if you had 300 rows but you edited the row 1000 too the librarie take the row 1000.
                 break
             else:
                 print(ws.cell(row=x,column=2).value)
